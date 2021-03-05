70a3fae9f1a648f2fe69321da4b39c75bc6c4e01
David Martinez <davestringm@gmail.com>
2021-03-04 23:10:03 -0600

    add a Container component

---
+++ src/components/elements/Container/Container.tsx
@@ -0,0 +1,32 @@
+import { ReactElement, ReactNode } from 'react';
+
+import { Box, BoxProps } from '@chakra-ui/react';
+
+export interface Props extends BoxProps {
+  children: ReactNode;
+  isFluid?: boolean;
+}
+
+function Container({ children, isFluid, ...rest }: Props): ReactElement {
+  return (
+    <Box
+      {...{
+        marginX: 'auto',
+        maxWidth: '1200px',
+        paddingX: 4,
+        position: 'relative',
+        width: '100%',
+        ...(isFluid && { maxWidth: '100%', paddingX: 0 }),
+        ...rest,
+      }}
+    >
+      {children}
+    </Box>
+  );
+}
+
+Container.defaultProps = {
+  isFluid: false,
+};
+
+export default Container;
