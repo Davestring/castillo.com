61ff9cfed66480e745d462a12957cefc2420128d
David Martinez <davestringm@gmail.com>
2021-03-06 03:51:06 -0600

    update chakra style configuration files and remove TS extension and use JS

---
+++ src/styles/colors.js
@@ -0,0 +1,7 @@
+const colors = {
+  base: '#101d2c',
+  primary: '#f9f7f6',
+  white: '#ffffff',
+};
+
+export default colors;
---
+++ src/styles/fonts.js
@@ -0,0 +1,7 @@
+const fonts = {
+  body: "'Helvetica Neue', sans-serif",
+  heading: "'Helvetica Neue', sans-serif",
+  mono: "'Garamond', monospace",
+};
+
+export default fonts;
