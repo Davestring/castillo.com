e0eff3f67d883f3ff6646405b133f0e95ec3a43e
David Martinez <davestringm@gmail.com>
2021-03-06 03:53:33 -0600

    update the Container component

---
+++ src/components/elements/Container/Container.js
@@ -0,0 +1,32 @@
+import { Box } from '@chakra-ui/react';
+import PropTypes from 'prop-types';
+
+function Container(props) {
+  const { children, isFluid, ...rest } = props;
+  return (
+    <Box
+      {...{
+        marginX: 'auto',
+        maxWidth: '1200px',
+        paddingX: 4,
+        position: 'relative',
+        width: '100%',
+        ...(isFluid && { maxWidth: '100%', paddingX: 0 }),
+        ...rest,
+      }}
+    >
+      {children}
+    </Box>
+  );
+}
+
+Container.propTypes = {
+  children: PropTypes.element,
+  isFluid: PropTypes.bool,
+};
+
+Container.defaultProps = {
+  isFluid: false,
+};
+
+export default Container;
