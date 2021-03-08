7267ea540711fba6781ccb1a93c1eec9954405c0
David Martinez <davestringm@gmail.com>
2021-03-08 00:13:50 -0600

    implement the Service and Features components in the HomePage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,12 @@
 import { Box } from '@chakra-ui/react';
+import Features from 'components/modules/Features';
 import Layout from 'components/modules/Layout';
+import Service from 'components/modules/Service';
 import Spotlight from 'components/modules/Spotlight';
+import { FEATURES_DATA } from 'mocks/features';
+import { SERVICES_DATA } from 'mocks/services';
+import { SPOTLIGHT_HEADER, SPOTLIGHT_SUB_HEADER } from 'mocks/spotlight';
+import { nanoid } from 'nanoid';
 import Head from 'next/head';
 
 function CastilloCasa() {
@@ -11,7 +17,22 @@ function CastilloCasa() {
       </Head>
       <Layout>
         <Box as="article" mt="-4rem">
-          <Spotlight as="section" bg="base"></Spotlight>
+          <Spotlight
+            as="section"
+            bg="base"
+            title={SPOTLIGHT_HEADER}
+            subtitle={SPOTLIGHT_SUB_HEADER}
+          ></Spotlight>
+          <Box as="section">
+            {SERVICES_DATA.map((item) => (
+              <Service key={nanoid()} my={12} {...item}></Service>
+            ))}
+          </Box>
+          <Features
+            as="section"
+            bg="primary"
+            features={FEATURES_DATA}
+          ></Features>
         </Box>
       </Layout>
     </>
