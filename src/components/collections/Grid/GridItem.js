2581e5e4c60181944317074d105b0bce558bb405
David Martinez <davestringm@gmail.com>
2021-03-08 15:14:12 -0600

    create a custom grid component

---
+++ src/components/collections/Grid/GridItem.js
@@ -0,0 +1,30 @@
+import { Box } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+import { isNil } from 'components/lib/children';
+import PropTypes from 'prop-types';
+
+const Item = styled(Box)`
+  grid-column: ${({ column }) => column};
+  grid-row: ${({ row }) => row};
+`;
+
+function GridItem(props) {
+  const { children, column, row, ...rest } = props;
+
+  if (isNil(children)) return null;
+  return (
+    <Item column={column} row={row} {...rest}>
+      {children}
+    </Item>
+  );
+}
+
+GridItem.propTypes = {
+  children: PropTypes.elementType,
+  column: PropTypes.string,
+  row: PropTypes.string,
+};
+
+GridItem.defaultProps = {};
+
+export default GridItem;
