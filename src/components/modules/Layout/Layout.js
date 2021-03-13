1ee5edfe6736548834892a0fd89e4916b6ac89d1
David Martinez <davestringm@gmail.com>
2021-03-12 23:50:57 -0600

    implement the Footer component in the application Layout

--- src/components/modules/Layout/Layout.js
+++ src/components/modules/Layout/Layout.js
@@ -1,13 +1,39 @@
+import Footer from 'components/modules/Footer';
 import PropTypes from 'prop-types';
+import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
 
 import ISOContainer from './components/ISOContainer';
 import Wrapper from './components/Wrapper';
+
+const SOCIAL_SHARE = [
+  {
+    id: 'fb',
+    url: 'https://facebook.com/',
+    icon: FaFacebookF,
+  },
+  {
+    id: 'ig',
+    url: 'https://instagram.com/',
+    icon: FaInstagram,
+  },
+  {
+    id: 'tw',
+    url: 'https://twitter.com/',
+    icon: FaTwitter,
+  },
+  {
+    id: 'yt',
+    url: 'https://youtube.com/',
+    icon: FaYoutube,
+  },
+];
 
 function Layout(props) {
   const { children, ...rest } = props;
   return (
     <Wrapper {...rest}>
       <ISOContainer as="main">{children}</ISOContainer>
+      <Footer as="footer" social={SOCIAL_SHARE} mb={4} mt={12}></Footer>
     </Wrapper>
   );
 }
