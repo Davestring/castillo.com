365bdc605688dba85eda744791b83e6544a9a740
David Martinez <davestringm@gmail.com>
2021-03-12 18:01:33 -0600

    add some styles to the H2 component

--- src/components/elements/H2/H2.js
+++ src/components/elements/H2/H2.js
@@ -4,14 +4,22 @@ import PropTypes from 'prop-types';
 function H2(props) {
   const { children, ...rest } = props;
   return (
-    <Heading as="h2" fontSize="xl" mb={4} {...rest}>
+    <Heading
+      as="h2"
+      fontSize="2xl"
+      fontWeight={200}
+      letterSpacing="5px"
+      mb={4}
+      textTransform="uppercase"
+      {...rest}
+    >
       {children}
     </Heading>
   );
 }
 
 H2.propTypes = {
-  children: PropTypes.elementType,
+  children: PropTypes.node,
 };
 
 export default H2;
