c78c8df70c2c0abeb885d39103ca0e617656058e
David Martinez <davestringm@gmail.com>
2021-03-19 02:42:52 -0600

    delete the mock data and implement the intl useTranslation hook in the Home page in order to remove hardcoded data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,24 @@
 import { Box } from '@chakra-ui/react';
-import Gallery from 'components/modules/Gallery';
+import { Head } from 'components/behaviours/Shareability';
 import Layout from 'components/modules/Layout';
-import Services from 'components/modules/Services';
 import Spotlight from 'components/modules/Spotlight';
-import { GALLERY_DATA, GALLERY_HEADING } from 'mocks/gallery';
-import { SERVICES_DATA, SERVICES_HEADING } from 'mocks/services';
-import { SPOTLIGHT_HEADER, SPOTLIGHT_QUOTE } from 'mocks/spotlight';
-import Head from 'next/head';
+import { useTranslations } from 'locales';
 
 function CastilloCasa() {
+  const { t } = useTranslations();
+
   return (
     <>
-      <Head>
-        <title>Casa Castillo</title>
-      </Head>
+      <Head title={t('meta.title')} description={t('meta.description')}></Head>
+
       <Layout>
         <Box as="article" mt="-4rem">
           <Spotlight
             as="section"
             bg="base"
-            title={SPOTLIGHT_HEADER}
-            quote={SPOTLIGHT_QUOTE}
+            title={t('spotlight.title')}
+            quote={t('spotlight.quote')}
           ></Spotlight>
-
-          <Services
-            as="section"
-            maxWidth="900px"
-            mt={20}
-            services={SERVICES_DATA}
-            heading={SERVICES_HEADING}
-          ></Services>
-
-          <Gallery
-            as="section"
-            content={GALLERY_DATA}
-            heading={GALLERY_HEADING}
-            mt={20}
-          ></Gallery>
         </Box>
       </Layout>
     </>
