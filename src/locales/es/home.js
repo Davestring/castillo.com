8bab3c39851932b5e3498b50e534e5a4d4f21b61
David Martinez <davestringm@gmail.com>
2021-03-19 02:22:57 -0600

    create the spanish locales files

---
+++ src/locales/es/home.js
@@ -0,0 +1,14 @@
+export default {
+  '/': {
+    // Meta Tags.
+    'meta.title': 'Casa Castillo - Vive una experiencia inolvidable',
+    'meta.description': '',
+    // Spotlight.
+    'spotlight.title': '',
+    'spotlight.quote': '',
+    // Insights.
+    'insights.title': '',
+    // Gallery.
+    'gallery.title': '',
+  },
+};
