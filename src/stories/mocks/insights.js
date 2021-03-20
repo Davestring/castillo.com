5b78967b1f70240cfcdb331411a551ba202158d1
David Martinez <davidmartinez@MacBook-Pro-de-David.local>
2021-03-19 23:59:41 -0600

    export the mock data inside the stories folder since there is where it will be used by storybook

--- src/stories/mocks/insights.js
+++ src/stories/mocks/insights.js
@@ -1,4 +1,6 @@
-export const SERVICES_HEADING = 'Neque porro quisquam est qui dolorem ipsum';
+import { IMAGE_330X440 } from './images';
+
+export const INSIGHTS_HEADING = 'Neque porro quisquam est qui dolorem ipsum';
 
 export const TITLE_HELPER = 'Sed ut perspiciatis unde omnis iste natus';
 
@@ -7,25 +9,24 @@ export const CAPTION_HELPER = 'At vero eos et accusamus et iusto';
 export const SUMMARY_HELPER =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
 
-export const SERVICES_DATA = [
+export const INSIGHTS_DATA = [
   {
     alt: 'service-mock-image',
-    image: '/images/mocks/330x440.png',
+    image: IMAGE_330X440,
     caption: CAPTION_HELPER,
     title: TITLE_HELPER,
     summary: SUMMARY_HELPER,
   },
   {
     alt: 'service-mock-image',
-    image: '/images/mocks/330x440.png',
+    image: IMAGE_330X440,
     caption: CAPTION_HELPER,
     title: TITLE_HELPER,
     summary: SUMMARY_HELPER,
-    isReverse: true,
   },
   {
     alt: 'service-mock-image',
-    image: '/images/mocks/330x440.png',
+    image: IMAGE_330X440,
     caption: CAPTION_HELPER,
     title: TITLE_HELPER,
     summary: SUMMARY_HELPER,
