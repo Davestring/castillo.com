a815b5b82fb856f0e46588a83c25d5cace0243cf
David Martinez <davestringm@gmail.com>
2021-03-30 00:08:48 -0600

    add the Insights component to the HomePage and retrieve the insights data from the API

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import { Box } from '@chakra-ui/react';
 import { Head } from 'components/behaviours/Shareability';
+import Insights from 'components/modules/Insights';
 import Layout from 'components/modules/Layout';
 import Spotlight from 'components/modules/Spotlight';
 import { useTranslations } from 'locales';
+import PropTypes from 'prop-types';
+import InsightResources from 'services/resources/insight';
 
-function CastilloCasa() {
+function Index(props) {
+  const { insights, error } = props;
   const { t } = useTranslations();
+
+  if (error) return null;
 
   return (
     <>
@@ -16,13 +22,61 @@ function CastilloCasa() {
           <Spotlight
             as="section"
             bg="base"
+            mb={20}
             title={t('spotlight.title')}
             quote={t('spotlight.quote')}
           ></Spotlight>
+          <Insights
+            as="section"
+            insights={insights}
+            maxWidth="900px"
+            mb={20}
+          ></Insights>
         </Box>
       </Layout>
     </>
   );
 }
 
-export default CastilloCasa;
+Index.propTypes = {
+  error: PropTypes.bool,
+  insights: PropTypes.arrayOf(
+    PropTypes.shape({
+      caption: PropTypes.string,
+      cover: PropTypes.oneOfType([PropTypes.string, PropTypes.elementType]),
+      summary: PropTypes.string,
+      title: PropTypes.string,
+      isReverse: PropTypes.bool,
+    }),
+  ),
+};
+
+Index.defaultProps = {
+  error: false,
+  insights: [],
+};
+
+export const getStaticProps = async () => {
+  try {
+    const response = await InsightResources.findAll();
+    const insights = response?.data?.map((item, idx) => ({
+      ...item,
+      isReverse: idx % 2 !== 0,
+    }));
+
+    return {
+      props: {
+        error: false,
+        insights,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        error: true,
+      },
+    };
+  }
+};
+
+export default Index;
