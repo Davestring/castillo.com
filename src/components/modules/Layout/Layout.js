09649870f83dc95cee1d294aca53b0d3288d9d89
David Martinez <davestringm@gmail.com>
2021-03-31 19:13:57 -0600

    update the Layout component and make it look cleaner

--- src/components/modules/Layout/Layout.js
+++ src/components/modules/Layout/Layout.js
@@ -1,28 +1,9 @@
 import Footer from 'components/modules/Footer';
 import Header from 'components/modules/Header';
 import PropTypes from 'prop-types';
-import { FaFacebookF, FaInstagram, FaYoutube } from 'react-icons/fa';
 
 import ISOContainer from './components/ISOContainer';
 import Wrapper from './components/Wrapper';
-
-const SOCIAL_SHARE = [
-  {
-    id: 'fb',
-    url: 'https://facebook.com/',
-    icon: FaFacebookF,
-  },
-  {
-    id: 'ig',
-    url: 'https://instagram.com/',
-    icon: FaInstagram,
-  },
-  {
-    id: 'yt',
-    url: 'https://youtube.com/',
-    icon: FaYoutube,
-  },
-];
 
 function Layout(props) {
   const { children, translations: t, ...rest } = props;
@@ -30,12 +11,7 @@ function Layout(props) {
     <Wrapper {...rest}>
       <Header as="header"></Header>
       <ISOContainer as="main">{children}</ISOContainer>
-      <Footer
-        as="footer"
-        social={SOCIAL_SHARE}
-        mb={4}
-        translations={t}
-      ></Footer>
+      <Footer as="footer" mb={4} translations={t}></Footer>
     </Wrapper>
   );
 }
