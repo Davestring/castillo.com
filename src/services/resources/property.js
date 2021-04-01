dab226bc2e09dd7acf1d980a65236d5300331b91
David Martinez <davestringm@gmail.com>
2021-03-31 19:18:01 -0600

    add the service to retrieve a property comments, networks and services

--- src/services/resources/property.js
+++ src/services/resources/property.js
@@ -3,7 +3,16 @@ import instance from 'services';
 const resource = {
   findAll: (params) => instance.get('/house/', params),
 
-  findByID: (id) => instance.get(`/house/${id}`),
+  findByID: (id) => instance.get(`/house/${id}`).then((res) => res.data),
+
+  findPropertyComments: (id) =>
+    instance.get(`/house/${id}/comments/`).then((res) => res.data),
+
+  findPropertyNetworks: (id) =>
+    instance.get(`/house/${id}/networks/`).then((res) => res.data),
+
+  findPropertyServices: (id) =>
+    instance.get(`/house/${id}/services/`).then((res) => res.data),
 };
 
 export default resource;
