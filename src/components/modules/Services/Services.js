86f42f15bd8bd934f72a6cbe33f4ab2ed1a833ef
David Martinez <davestringm@gmail.com>
2021-03-31 19:15:49 -0600

    add a button to display all services, at this moment this button will do nothing

--- src/components/modules/Services/Services.js
+++ src/components/modules/Services/Services.js
@@ -1,10 +1,10 @@
-import { SimpleGrid } from '@chakra-ui/layout';
+import { Button, Flex, SimpleGrid } from '@chakra-ui/react';
 import Container from 'components/elements/Container';
 import _ from 'lodash';
 import { nanoid } from 'nanoid';
 import PropTypes from 'prop-types';
 
-import ServiceCard from './components/ServiceCard';
+import Service from './components/Service';
 
 function Services(props) {
   const { services, ...rest } = props;
@@ -14,11 +14,16 @@ function Services(props) {
   return (
     <Container isFluid {...rest}>
       <Container py={20}>
-        <SimpleGrid columns={[{ base: 1, md: 2, lg: 3 }]} spacing={12}>
+        <SimpleGrid columns={[1, 1, 2, 3]} spacing={12}>
           {services.map((item) => (
-            <ServiceCard key={nanoid()} {...item}></ServiceCard>
+            <Service key={nanoid()} {...item}></Service>
           ))}
         </SimpleGrid>
+        <Flex justify="center" mt={12}>
+          <Button bg="gold" color="white" px={8} size="sm">
+            Ver todos los servicios
+          </Button>
+        </Flex>
       </Container>
     </Container>
   );
@@ -30,7 +35,6 @@ Services.propTypes = {
       icon: PropTypes.elementType,
       title: PropTypes.string,
       description: PropTypes.string,
-      iconColor: PropTypes.string,
     }),
   ),
 };
