b13b2b961eff4a16ef9737837d6c085540ae927a
David Martinez <davestringm@gmail.com>
2021-03-31 19:17:29 -0600

    update the getStaticProps function in the Properties page

--- src/pages/properties/index.js
+++ src/pages/properties/index.js
@@ -4,15 +4,27 @@ import Layout from 'components/modules/Layout';
 import Listing from 'components/modules/Properties';
 import Spotlight from 'components/modules/Spotlight';
 import { useTranslations } from 'locales';
+import _ from 'lodash';
 import PropTypes from 'prop-types';
 import PropertyResources from 'services/resources/property';
 import formatAddress from 'utils/format-address';
 
-function Properties(props) {
-  const { properties, error } = props;
-  const { t } = useTranslations();
+export const getStaticProps = async () => {
+  const formatObj = (obj) => ({ ...obj, address: formatAddress(obj?.address) });
 
-  if (error) return null;
+  const response = await PropertyResources.findAll();
+  const properties = _.flow(
+    (obj) => _.get(obj, 'data'),
+    (arr) => _.map(arr, (obj) => formatObj(obj)),
+    (arr) => _.map(arr, (obj) => _.omit(obj, ['active', 'created', 'updated'])),
+  )(response);
+
+  return { props: { properties } };
+};
+
+function Properties(props) {
+  const { properties } = props;
+  const { t } = useTranslations();
 
   return (
     <>
@@ -36,44 +48,20 @@ function Properties(props) {
 }
 
 Properties.propTypes = {
-  error: PropTypes.bool,
   properties: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number,
-      address: PropTypes.string,
-      cover: PropTypes.string,
-      description: PropTypes.string,
       title: PropTypes.string,
+      description: PropTypes.string,
+      cover: PropTypes.string,
+      rating: PropTypes.number,
+      address: PropTypes.string,
     }),
   ),
 };
 
 Properties.defaultProps = {
-  error: false,
   properties: [],
-};
-
-export const getStaticProps = async () => {
-  try {
-    const response = await PropertyResources.findAll();
-    const properties = response?.data?.map((item) => ({
-      ...item,
-      address: formatAddress(item.address),
-    }));
-
-    return {
-      props: {
-        error: false,
-        properties,
-      },
-    };
-  } catch (error) {
-    return {
-      props: {
-        error: true,
-      },
-    };
-  }
 };
 
 export default Properties;
