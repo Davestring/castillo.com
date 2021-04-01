b58d106c39239d9d6cea5f9c090a8952f0405833
David Martinez <davestringm@gmail.com>
2021-03-31 19:12:28 -0600

    update the H1 and H2 components to look cleaner

--- src/components/elements/H1/H1.js
+++ src/components/elements/H1/H1.js
@@ -4,15 +4,7 @@ import PropTypes from 'prop-types';
 function H1(props) {
   const { children, ...rest } = props;
   return (
-    <Heading
-      as="h1"
-      fontSize="4xl"
-      fontWeight={200}
-      letterSpacing="10px"
-      mb={4}
-      textTransform="uppercase"
-      {...rest}
-    >
+    <Heading as="h1" {...rest}>
       {children}
     </Heading>
   );
@@ -20,6 +12,14 @@ function H1(props) {
 
 H1.propTypes = {
   children: PropTypes.node,
+};
+
+H1.defaultProps = {
+  fontSize: '4xl',
+  fontWeight: 200,
+  letterSpacing: '10px',
+  mb: 4,
+  textTransform: 'uppercase',
 };
 
 export default H1;
