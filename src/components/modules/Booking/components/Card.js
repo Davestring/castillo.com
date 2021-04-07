0c4ed0d4ed81703d9e52ebd9f5306bff40be17e7
David Martinez <davestringm@gmail.com>
2021-04-07 17:04:54 -0500

    add flex styles to Booking Card

--- src/components/modules/Booking/components/Card.js
+++ src/components/modules/Booking/components/Card.js
@@ -6,6 +6,8 @@ const Card = styled(Box)`
   border-color: ${({ theme }) => theme.colors.blackAlpha[200]};
   border-radius: ${({ theme }) => theme.radii.md};
   box-shadow: ${({ theme }) => theme.shadows.md};
+  flex-direction: column;
+  justify-content: space-between;
   max-height: ${({ theme }) => theme.sizes['2xs']};
   margin-left: ${({ theme }) => theme.space[12]};
   padding: ${({ theme }) => theme.space[8]};
