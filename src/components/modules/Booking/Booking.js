c7d529285a42de19b0b5e54cd6b3492d5ae780f9
David Martinez <davestringm@gmail.com>
2021-04-07 17:04:38 -0500

    add the booking button in the Booking component

--- src/components/modules/Booking/Booking.js
+++ src/components/modules/Booking/Booking.js
@@ -1,26 +1,19 @@
-import { Icon, Stack, Text } from '@chakra-ui/react';
+import { Box, Button } from '@chakra-ui/react';
 import Calendar from 'components/collections/Calendar';
 import PropTypes from 'prop-types';
-import { FaStar } from 'react-icons/fa';
 
 import Card from './components/Card';
+import Header from './components/Header';
 
 function Booking(props) {
   const { price, rating, ...rest } = props;
   return (
-    <Card d={{ base: 'none', lg: 'block' }} {...rest}>
-      <Stack isInline align="center" justify="space-between" mb={4}>
-        <Text fontSize="lg" fontWeight={700}>
-          ${price} MXN
-        </Text>
-        <Stack isInline align="center" spacing={1}>
-          <Icon as={FaStar} color="gold" h={4} mt="-4px" w={4}></Icon>
-          <Text as="span" fontSize="sm">
-            {rating}
-          </Text>
-        </Stack>
-      </Stack>
-      <Calendar></Calendar>
+    <Card d={{ base: 'none', lg: 'flex' }} {...rest}>
+      <Box>
+        <Header price={price} rating={rating}></Header>
+        <Calendar></Calendar>
+      </Box>
+      <Button colorScheme="golden">Reserva</Button>
     </Card>
   );
 }
