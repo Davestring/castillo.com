d790b29aec330668386574e7126a95c960e84b45
David Martinez <dhernandez@applaudostudios.com>
2022-10-11 00:53:04 -0500

    chore(v2): initialize react project

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import { App } from 'pages/App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const MOUNT_NODE = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
+const CasaCastillo: React.FC = (): JSX.Element => (
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>
+);
+
+MOUNT_NODE.render(<CasaCastillo />);
---
+++ src/pages/App/App.tsx
@@ -0,0 +1,5 @@
+export const App = (): JSX.Element => {
+    return (
+        <h1>Casa Castillo!</h1>
+    )
+}
