ccd0978007ed7c9908985c9130d7d2b1b8651d39
David Martinez <dhernandez@applaudostudios.com>
2022-10-11 21:07:15 -0500

    fix: change helmet location in order to render preview in social media

--- src/components/layout/SEO/SEO.tsx
+++ src/components/layout/SEO/SEO.tsx
@@ -6,14 +6,10 @@ export interface ISEOProps {
    * SEO cover image.
    */
   cover?: string;
-  /**
-   * SEO title.
-   */
-  title: string;
 }
 
 export const SEO: React.FC<ISEOProps> = (props): JSX.Element => {
-  const { cover = 'assets/img/meta.jpg', title } = props;
+  const { cover = 'assets/img/meta.jpg' } = props;
 
   const { t } = useTranslation('common');
 
@@ -24,19 +20,18 @@ export const SEO: React.FC<ISEOProps> = (props): JSX.Element => {
       <meta content="https://casa-castillo.netlify.app" property="og:url" />
       <meta content="casa-castillo" property="og:image:alt" />
       <meta content="image/jpg" property="og:image:type" />
-      <meta content={t('meta')} property="og:description" />
+      <meta content={t('meta.description')} property="og:description" />
+      <meta content={t('meta.title')} property="og:site_name" />
+      <meta content={t('meta.title')} property="og:title" />
+      <meta content={cover} property="og:image" />
 
       <meta content="summary" name="twitter:card" />
-      <meta content={t('meta')} name="twitter:description" />
+      <meta content={t('meta.description')} name="twitter:description" />
+      <meta content={t('meta.title')} name="twitter:title" />
+      <meta content={cover} name="twitter:image" />
 
-      {cover && <meta content={cover} property="og:image" />}
-      {title && <meta content={title} property="og:site_name" />}
-      {title && <meta content={title} property="og:title" />}
-      {cover && <meta content={cover} name="twitter:image" />}
-      {title && <meta content={title} name="twitter:title" />}
-
-      <meta content={t('meta')} name="description" />
-      <title>{title}</title>
+      <meta content={t('meta.description')} name="description" />
+      <title>{t('meta.title')}</title>
     </Helmet>
   );
 };
--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,10 +1,15 @@
+import { SEO } from 'components/layout';
 import { HomePage } from 'pages/Home';
 import { NotFoundPage } from 'pages/NotFound';
 import { Route, Routes } from 'react-router-dom';
 
 export const App = (): JSX.Element => (
-  <Routes>
-    <Route element={<HomePage />} path="/" />
-    <Route element={<NotFoundPage />} path="*" />
-  </Routes>
+  <>
+    <SEO />
+
+    <Routes>
+      <Route element={<HomePage />} path="/" />
+      <Route element={<NotFoundPage />} path="*" />
+    </Routes>
+  </>
 );
