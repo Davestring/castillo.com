c8f4dc354e1536c3bead76ddc1b183935e69c6d8
David Martinez <dhernandez@applaudostudios.com>
2022-10-11 20:54:20 -0500

    fix: remove nested components from helmet wrapper

--- src/components/layout/SEO/SEO.tsx
+++ src/components/layout/SEO/SEO.tsx
@@ -1,9 +1,6 @@
 import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
 
-import { OpenGraphMeta } from './components/OpenGraphMeta';
-import { TwitterMeta } from './components/TwitterMeta';
-
 export interface ISEOProps {
   /**
    * SEO cover image.
@@ -22,10 +19,24 @@ export const SEO: React.FC<ISEOProps> = (props): JSX.Element => {
 
   return (
     <Helmet>
-      <title>{title}</title>
+      <meta content="" property="fb:app_id" />
+      <meta content="website" property="og:type" />
+      <meta content="https://casa-castillo.netlify.app" property="og:url" />
+      <meta content="casa-castillo" property="og:image:alt" />
+      <meta content="image/jpg" property="og:image:type" />
+      <meta content={t('meta')} property="og:description" />
+
+      <meta content="summary" name="twitter:card" />
+      <meta content={t('meta')} name="twitter:description" />
+
+      {cover && <meta content={cover} property="og:image" />}
+      {title && <meta content={title} property="og:site_name" />}
+      {title && <meta content={title} property="og:title" />}
+      {cover && <meta content={cover} name="twitter:image" />}
+      {title && <meta content={title} name="twitter:title" />}
+
       <meta content={t('meta')} name="description" />
-      <OpenGraphMeta cover={cover} description={t('meta')} title={title} />
-      <TwitterMeta cover={cover} description={t('meta')} title={title} />
+      <title>{title}</title>
     </Helmet>
   );
 };
