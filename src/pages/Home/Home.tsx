12aa180d03dab091497946dc24ed742e47b9fd27
David Martinez <dhernandez@applaudostudios.com>
2022-10-11 21:40:45 -0500

    feature: home page will use default title tag

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,3 @@
-import { Helmet } from 'react-helmet-async';
-import { useTranslation } from 'react-i18next';
-
 import { Spotlight } from './components/Spotlight';
 
-export const Home = (): JSX.Element => {
-  const { t } = useTranslation('page:home');
-
-  return (
-    <>
-      <Helmet>
-        <title>{t('helmet')}</title>
-      </Helmet>
-
-      <Spotlight />
-    </>
-  );
-};
+export const Home = (): JSX.Element => <Spotlight />;
