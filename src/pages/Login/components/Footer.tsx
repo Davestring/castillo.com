2ac9d36ff65531b86c2fdd617dfe46ce526f1423
David Martinez <dhernandez@applaudostudios.com>
2022-10-13 18:28:00 -0500

    feature: create the login Footer component

---
+++ src/pages/Login/components/Footer.tsx
@@ -0,0 +1,48 @@
+import { Divider, Link, Stack, StackProps } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
+import { Link as ReachLink } from 'react-router-dom';
+
+export type IFooterProps = StackProps;
+
+export const Footer: React.FC<IFooterProps> = (props): JSX.Element => {
+  const { t } = useTranslation('common');
+
+  return (
+    <Stack as="footer" {...props}>
+      <Link
+        as={ReachLink}
+        color="blue.700"
+        fontSize="xs"
+        fontWeight="semibold"
+        to="/privacy"
+      >
+        {t('privacy')}
+      </Link>
+
+      <Divider bg="blue.700" h={4} orientation="vertical" w="0.125rem" />
+
+      <Link
+        as={ReachLink}
+        color="blue.700"
+        fontSize="xs"
+        fontWeight="semibold"
+        to="/terms"
+      >
+        {t('terms')}
+      </Link>
+    </Stack>
+  );
+};
+
+Footer.defaultProps = {
+  align: 'center',
+  bottom: 6,
+  gap: 4,
+  isInline: true,
+  justify: 'center',
+  left: '50%',
+  position: 'fixed',
+  transform: 'translateX(-50%)',
+  w: '100%',
+  zIndex: 'docked',
+};
