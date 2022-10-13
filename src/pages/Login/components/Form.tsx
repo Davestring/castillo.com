9b6c4a817f7cc7a767c1d7aabe574a0313f05324
David Martinez <dhernandez@applaudostudios.com>
2022-10-13 18:24:18 -0500

    feature: create the Login form component

---
+++ src/pages/Login/components/Form.tsx
@@ -0,0 +1,87 @@
+import { Button, Link, Stack, StackProps, Text } from '@chakra-ui/react';
+import { InputField } from 'components/inputs';
+import { Form as ReachForm, Formik, FormikHelpers } from 'formik';
+import { Trans, useTranslation } from 'react-i18next';
+import { Link as ReachLink } from 'react-router-dom';
+import * as Yup from 'yup';
+
+const INITIAL_VALUES = { email: '', password: '' };
+
+const VALIDATION_SCHEMA = Yup.object().shape({
+  email: Yup.string()
+    .email('form.generic.email')
+    .required('form.generic.required'),
+
+  password: Yup.string()
+    .min(8, 'form.generic.min-length')
+    .required('form.generic.required'),
+});
+
+export interface IFormProps extends Omit<StackProps, 'onSubmit'> {
+  /**
+   * Form initial values, a IBaseAuthentication object.
+   */
+  initialValues?: { email: string; password: string };
+  /**
+   * Action that will be triggered when the user submits the form.
+   */
+  onSubmit: (
+    v: { email: string; password: string },
+    helpers?: FormikHelpers<{ email: string; password: string }>,
+  ) => void;
+}
+
+export const Form: React.FC<IFormProps> = (props): JSX.Element => {
+  const { initialValues = INITIAL_VALUES, onSubmit, ...rest } = props;
+
+  const { t } = useTranslation('page:login');
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={VALIDATION_SCHEMA}
+    >
+      <Stack as={ReachForm} {...rest}>
+        <InputField
+          name="email"
+          placeholder={t('form.email.placeholder')}
+          size="md"
+        />
+
+        <InputField
+          name="password"
+          placeholder={t('form.password.placeholder')}
+          size="md"
+        />
+
+        <Button colorScheme="facebook" size="md" type="submit">
+          {t('button.continue', { ns: 'common' })}
+        </Button>
+
+        <Text align="center">
+          <Trans i18nKey="notice" ns="page:login">
+            <Text
+              as="span"
+              color="blackAlpha.500"
+              fontSize="xs"
+              fontWeight="medium"
+            />
+            <Link
+              as={ReachLink}
+              color="blue.700"
+              fontSize="xs"
+              fontWeight="bold"
+              mt={0}
+              to="/booking"
+            />
+          </Trans>
+        </Text>
+      </Stack>
+    </Formik>
+  );
+};
+
+Form.defaultProps = {
+  gap: 4,
+};
