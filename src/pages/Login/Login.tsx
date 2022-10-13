12d760e901bab87455e87b3f332e0f7646128153
David Martinez <dhernandez@applaudostudios.com>
2022-10-13 18:29:32 -0500

    feature: complete the login page UI

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,3 +1,65 @@
-import { H1 } from 'components/typography';
+import { Text } from '@chakra-ui/react';
+import Color from 'color';
+import { AnalyticsIcon } from 'components/icons';
+import { Container } from 'components/layout';
+import { useCallback } from 'react';
+import { use100vh } from 'react-div-100vh';
+import { Helmet } from 'react-helmet-async';
+import { useTranslation } from 'react-i18next';
 
-export const Login = (): JSX.Element => <H1>Login!</H1>;
+import { Card } from './components/Card';
+import { Footer } from './components/Footer';
+import { Form } from './components/Form';
+import { Header } from './components/Header';
+
+export const Login = (): JSX.Element => {
+  const { t } = useTranslation('page:login');
+
+  const height = use100vh();
+
+  const handleOnSubmit = useCallback(() => {}, []);
+
+  return (
+    <>
+      <Helmet>
+        <title>{t('helmet')}</title>
+      </Helmet>
+
+      <Container
+        bg={{ base: 'white', md: Color('#101D2C').lighten(7).hex() }}
+        isFluid
+        minH={height || 0}
+        py={12}
+      >
+        <Header mb={{ base: 8, md: 12 }} />
+
+        <Card>
+          <Text
+            align="center"
+            color="blue.700"
+            fontSize="lg"
+            fontWeight="bold"
+            mb={8}
+          >
+            {t('title')}
+          </Text>
+
+          <Form onSubmit={handleOnSubmit} />
+        </Card>
+
+        <Footer />
+      </Container>
+
+      <AnalyticsIcon
+        bottom={8}
+        color="blue.700"
+        display={{ base: 'none', lg: 'block' }}
+        height={226}
+        position="fixed"
+        right={8}
+        width={341}
+        zIndex="docked"
+      />
+    </>
+  );
+};
---
+++ src/pages/Login/components/Card.tsx
@@ -0,0 +1,24 @@
+import { Box, BoxProps } from '@chakra-ui/react';
+
+export type ICardProps = BoxProps;
+
+export const Card: React.FC<ICardProps> = (props): JSX.Element => {
+  const { children, ...rest } = props;
+
+  return (
+    <Box as="section" {...rest}>
+      {children}
+    </Box>
+  );
+};
+
+Card.defaultProps = {
+  bg: 'white',
+  mx: 'auto',
+  p: { base: 0, md: 8 },
+  role: 'main',
+  rounded: { base: undefined, md: 'lg' },
+  shadow: { base: undefined, md: 'md' },
+  transition: 'all .5s ease',
+  w: { base: '100%', md: 480 },
+};
