70898b3aa626d66ca37f5d0ad93595683b630c2f
David Martinez <dhernandez@applaudostudios.com>
2022-10-14 16:27:14 -0500

    feature: remove slogan from login page

--- src/pages/Login/components/Header.tsx
+++ src/pages/Login/components/Header.tsx
@@ -1,27 +1,13 @@
 import { Flex, FlexProps, Image, Text } from '@chakra-ui/react';
 import Logo from 'assets/img/logo.dark.png';
-import { useTranslation } from 'react-i18next';
 
 export type IHeaderProps = FlexProps;
 
-export const Header: React.FC<IHeaderProps> = (props): JSX.Element => {
-  const { t } = useTranslation('page:login');
-
-  return (
-    <Flex as="header" {...props}>
-      <Image src={Logo} w={256} />
-
-      <Text
-        display={{ base: 'none', md: 'block' }}
-        fontSize="sm"
-        fontWeight="thin"
-        mt={2}
-      >
-        {t('slogan', { ns: 'common' })}
-      </Text>
-    </Flex>
-  );
-};
+export const Header: React.FC<IHeaderProps> = (props): JSX.Element => (
+  <Flex as="header" {...props}>
+    <Image src={Logo} w={256} />
+  </Flex>
+);
 
 Header.defaultProps = {
   align: 'center',
