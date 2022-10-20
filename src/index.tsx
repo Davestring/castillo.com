8125df149577580b78ac6694c26643479f97de78
David Martinez <dhernandez@applaudostudios.com>
2022-10-20 18:46:01 -0500

    feature: prepare the query provider

--- src/index.tsx
+++ src/index.tsx
@@ -10,9 +10,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { I18nextProvider } from 'react-i18next';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { GLOBAL_CSS, theme } from 'utils';
+
+const QUERY_CLIENT = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 const MOUNT_NODE = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -27,7 +36,9 @@ const CasaCastillo: React.FC = (): JSX.Element => (
         <Global styles={GLOBAL_CSS} />
         <BrowserRouter>
           <AuthProvider>
-            <App />
+            <QueryClientProvider client={QUERY_CLIENT}>
+              <App />
+            </QueryClientProvider>
           </AuthProvider>
         </BrowserRouter>
       </ChakraProvider>
